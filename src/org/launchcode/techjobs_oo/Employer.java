1bd153e703d74d1d95d228c0c753898a8d073c0c
miguelrodfherLearning <64111764+miguelrodfherLearning@users.noreply.github.com>
2020-07-15 15:27:04 -0400

    Add files via upload

--- src/org/launchcode/techjobs_oo/Employer.java
+++ src/org/launchcode/techjobs_oo/Employer.java
@@ -2,14 +2,15 @@ package org.launchcode.techjobs_oo;
 
 import java.util.Objects;
 
-public class Employer {
+public class Employer extends JobField  {
     private int id;
     private static int nextId = 1;
     private String value;
 
     public Employer() {
-        id = nextId;
-        nextId++;
+        super();
+//        id = nextId;
+//        nextId++;
     }
 
     public Employer(String value) {
--- src/org/launchcode/techjobs_oo/Job.java
+++ src/org/launchcode/techjobs_oo/Job.java
@@ -1,5 +1,8 @@
 package org.launchcode.techjobs_oo;
 
+import java.lang.reflect.Field;
+import java.util.Objects;
+
 public class Job {
 
     private int id;
@@ -11,13 +14,120 @@ public class Job {
     private PositionType positionType;
     private CoreCompetency coreCompetency;
 
-    // TODO: Add two constructors - one to initialize a unique ID and a second to initialize the
+    //TODO: Add two constructors - one to initialize a unique ID and a second to initialize the
     //  other five fields. The second constructor should also call the first in order to initialize
     //  the 'id' field.
 
-    // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
-    //  match.
+    public Job() {
+        id = nextId;
+        nextId++;
+    }
 
-    // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
-    //  and id.
+    public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
+        this();
+        this.name = name;
+        this.employer = employer;
+        this.location = location;
+        this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
+    }
+
+
+    @Override
+    public String toString() {
+        String[] labels = {"ID: ", "Name: ", "Employer: ", "Location: ", "Position Type: ", "Core Competency: "};
+        Field[] fields = Job.class.getDeclaredFields();
+        String unavailable = "Data not available";
+        String message = "\n";
+        int index = 0;
+
+        for (Field f : fields) {
+            if (f.getName() == "nextId") {
+                // don't do anything
+            } else {
+                try {
+                    // if this is a job field complete this task
+                    if (f.get(this) instanceof JobField) {
+                        // if this job field is not completed then do this
+                        if (((JobField) f.get(this)).getValue() == "") {
+
+                            message = message + labels[index] + unavailable + "\n";
+                        } else {
+                            message = message + labels[index] + f.get(this) + "\n";
+                        }
+                    } else if (f.get(this) == null || f.get(this) == "") {
+                        message = message + labels[index] + unavailable + "\n";
+                    } else {
+                        message = message + labels[index] + f.get(this) + "\n";
+                    }
+                    index++;
+                } catch (Exception e) {
+                    message = message + labels[index] + unavailable + "\n";
+                    index++;
+                }
+            }
+        }
+
+        return message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Job)) return false;
+        Job job = (Job) o;
+        return getId() == job.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    // Getters and Setters:
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(Employer value) {
+        this.employer = value;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location value) {
+        this.location = value;
+    }
+
+    public PositionType getPositionType() {
+        return positionType;
+    }
+
+    public void setPositionType(PositionType value) {
+        this.positionType = value;
+    }
+
+    public CoreCompetency getCoreCompetancy() {
+        return coreCompetency;
+    }
+
+    public void setCoreCompetancy(CoreCompetency value) {
+        this.coreCompetency = value;
+    }
+
 }
